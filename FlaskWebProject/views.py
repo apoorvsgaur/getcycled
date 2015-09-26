282b825e78c0c816d08362e0f888d0d9ee3cb167
Xiaoyang Kao <contact.xykao@gmail.com>
2015-09-26 17:27:55 -0500

    Test commit
    Testing pulls

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -12,7 +12,7 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='huuu',
+        title='Home',
         year=datetime.now().year,
     )
 
