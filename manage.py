9610cf1850515e3ceaa002088442f0c1fbf0fe11
Rohan Subramaniam <rcsubra2@illinois.edu>
2015-09-27 01:04:09 -0500

    __init__.py created, pray that this shit works

---
+++ manage.py
@@ -0,0 +1,18 @@
+from getcycled.FlaskWebProject import app, db
+from getcycled.FlaskWebProject.models import User
+from flask.ext.script import Manager, prompt_bool
+
+manager = Manager(app)
+
+@manager.command
+def initdb():
+	db.create_all()
+	db.session.commit()
+	print 'Initialized the database'
+@manager.command
+def dropdp():
+	if prompt_bool("WARNING: YOU WILL LOSE ALL YOUR DATA. Do you wish to continue?"):
+		db.drop_all()
+		print 'Dropped the database'
+if __name == '__main__':
+	manager.run()
